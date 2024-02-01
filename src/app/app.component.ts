ae368685283d11702e718ea58ee5930e31c49a80
unknown <suchacek.n@gmail.com>
2024-02-01 23:15:52 +0100

    Problem with input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Output, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'wage_calculator';
+  public netWorth: number = 0;
+  public input: number = 0;
+
+  constructor() {}
+
+  public calcNetWorth() {
+    console.log(this.input);
+    let HealthInsurance: number = this.input * 0.045;
+    let SocialInsurance: number = this.input * 0.071;
+    let IncomeTax: number = this.input * 0.15;
+    let TaxDiscount: number = 2570;
+    this.netWorth = this.input - HealthInsurance - SocialInsurance - IncomeTax + TaxDiscount;
+  }
+
 }
