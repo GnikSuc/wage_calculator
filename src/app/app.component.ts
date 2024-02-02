2c6644e5974c7a6748e75ac0188c924635ed1785
unknown <suchacek.n@gmail.com>
2024-02-02 01:26:03 +0100

    Problem solved, continued with project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,32 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   public netWorth: number = 0;
   public input: number = 0;
+  public HealthInsurance: number = 0;
+  public SocialInsurance: number = 0;
+  public IncomeTax: number = 0;
+  public TaxDiscount: number = 2570;
 
-  constructor() {}
 
-  public calcNetWorth() {
-    console.log(this.input);
-    let HealthInsurance: number = this.input * 0.045;
-    let SocialInsurance: number = this.input * 0.071;
-    let IncomeTax: number = this.input * 0.15;
-    let TaxDiscount: number = 2570;
-    this.netWorth = this.input - HealthInsurance - SocialInsurance - IncomeTax + TaxDiscount;
+  public calcHealthInsurance() {
+    this.HealthInsurance = Math.round(this.input * 0.045);
   }
 
+  public calcSociaInsurance() {
+    this.SocialInsurance = Math.round(this.input * 0.071);
+  }
+
+  public calcIncomeTax() {
+    this.IncomeTax = Math.round(this.input * 0.15);
+  }
+
+  public calcNetWorth() {
+    this.HealthInsurance = this.input * 0.045;
+    this.SocialInsurance = this.input * 0.071;
+    this.IncomeTax = this.input * 0.15;
+    this.netWorth = this.input - this.HealthInsurance - this.SocialInsurance - this.IncomeTax + this.TaxDiscount;
+  }
 }
