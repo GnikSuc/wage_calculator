7be2f846dd022bbcf3d3a792ef310d0bb6600475
unknown <suchacek.n@gmail.com>
2024-02-07 00:24:20 +0100

    Problem with kids is solved. Now it's time for disabled people.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,31 +12,41 @@ import { FormsModule } from '@angular/forms';
 })
 // const button = document.createElement('button');
 
-
 export class AppComponent {
 
   public inputWage: number = 0;
-  public inputKids: number = 0;
+  public inputNumKids: number = 0;
+  public inputNumKidsHandy: number = 0;
   // public input2: number = 0;
   
   public netWorth: number | string = 0;
   public HealthInsurance: number = 0;
   public SocialInsurance: number = 0;
-  public IncomeTax: number = 0;
-  public TaxDiscount: number = 0;
   public MinimalWage: number = 18900;
   public ErrorCatchPhrase: string = 'Zadejte mzdu vyšší než je minimální mzda ' + this.MinimalWage + ' Kč!';
 
+  public TaxDiscount: number = 0;
+  public TaxDiscountKids: number = 0;
+  public TaxDiscountKidsHandy: number = 0;
+  public TotalTaxDiscountKids: number = 0;
+  public TotalTaxDiscount: number = 0;
 
-  public numberOfKids: number[] = [1267, 1860, 2320];
+  public taxOfKids: number[] = [1267, 1860, 2320];
+  public taxOfKidsHandy: number[] = [2534, 3720, 4640];
+
+  public IncomeTax: number = 0;
+  public IncomeTaxRich: number = 131901;
+  public IncomeTaxPhrase: string = '(15 %)';
 
   onClick() {
     this.calcHealthInsurance();
     this.calcSociaInsurance();
     this.calcIncomeTax();
+    this.calcKidsTaxDiscount();
+    this.calcKidsHandyTaxDiscount();
+    this.countTaxKids();
     this.calcTaxDiscount();
     this.calcNetWorth();
-    // this.errorCatchPhase();  
   }
 
   public calcHealthInsurance() {
@@ -55,31 +65,56 @@ export class AppComponent {
     }
   }
 
+  //131901
   public calcIncomeTax() {
-    if (this.inputWage > this.MinimalWage) {
+    if (this.inputWage > this.MinimalWage && this.inputWage < this.IncomeTaxRich) {
     this.IncomeTax = Math.round(this.inputWage * 0.15);
+    } else if(this.inputWage >= this.IncomeTaxRich) {
+    this.IncomeTax = Math.round(this.inputWage * 0.23);
+    this.IncomeTaxPhrase = '(23 %) Solidární daň';
     } else {
-      this.IncomeTax = 0;
+    this.IncomeTax = 0;
     }
+  }
+
+  public calcKidsTaxDiscount() {
+    if (this.inputNumKids === 1) {
+      this.TaxDiscountKids = this.taxOfKids[0];
+    } else if (this.inputNumKids === 2) {
+      this.TaxDiscountKids = this.taxOfKids[0] + this.taxOfKids[1];
+    } else if (this.inputNumKids > 2) {
+      this.TaxDiscountKids = this.taxOfKids[0] + this.taxOfKids[1] + (this.taxOfKids[2]*(this.inputNumKids - 2));
+    } else {
+      this.TaxDiscountKids = 0;
+    }
+  }
+
+  public calcKidsHandyTaxDiscount() {
+    if (this.inputNumKidsHandy === 1) {
+      this.TaxDiscountKidsHandy = this.taxOfKidsHandy[0];
+    } else if (this.inputNumKidsHandy === 2) {
+      this.TaxDiscountKidsHandy = this.taxOfKidsHandy[0] + this.taxOfKidsHandy[1];
+    } else if (this.inputNumKidsHandy > 2) {
+      this.TaxDiscountKidsHandy = this.taxOfKidsHandy[0] + this.taxOfKidsHandy[1] + (this.taxOfKidsHandy[2]*(this.inputNumKidsHandy - 2));
+    } else {
+      this.TaxDiscountKidsHandy = 0;
+    }
+  }
+
+  public countTaxKids() {
+    this.TotalTaxDiscountKids = this.TaxDiscountKids + this.TaxDiscountKidsHandy;
   }
 
   public calcTaxDiscount() {
     if(this.inputWage > this.MinimalWage) {
-      // Výpočet bez započtení dětí
-      // this.TaxDiscount = 2570; 
-      
-      // Výpočet se započtením dětí
-      if (this.inputKids = 0) {
-        this.TaxDiscount = 2570;
-      } else if (this.inputKids = 1) {
-        this.TaxDiscount = 2570 + (this.numberOfKids[0]);
-      } else if (this.inputKids = 2) {
-        this.TaxDiscount = 2570 + (this.numberOfKids[0] + this.numberOfKids[1])
+      let kids = this.inputNumKids + this.inputNumKidsHandy;
+      if (kids < 1) {
+        this.TotalTaxDiscount = this.TaxDiscount = 2570;
       } else {
-        this.TaxDiscount = 2570 + (this.numberOfKids[0] + this.numberOfKids[1] + (this.numberOfKids[2]*(this.inputKids - 2)))
-      }
+        this.TotalTaxDiscount = (this.TaxDiscount = 2570) + this.TotalTaxDiscountKids;
+      } 
     } else {
-      this.TaxDiscount = 0;
+      this.TotalTaxDiscount = 0;
     }
   }
 
@@ -88,7 +123,7 @@ export class AppComponent {
     // this.HealthInsurance = this.inputWage * 0.045;
     // this.SocialInsurance = this.inputWage * 0.071;
     // this.IncomeTax = this.inputWage * 0.15;
-    this.netWorth = this.inputWage - this.HealthInsurance - this.SocialInsurance - this.IncomeTax + this.TaxDiscount;
+    this.netWorth = this.inputWage - this.HealthInsurance - this.SocialInsurance - this.IncomeTax + this.TotalTaxDiscount;
     } else {
       this.netWorth = this.ErrorCatchPhrase;
     }
