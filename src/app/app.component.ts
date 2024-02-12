ae3560b8e76328a72cbf7507c90d8e6ac7b77089
unknown <suchacek.n@gmail.com>
2024-02-12 20:42:32 +0100

    Problems solved, added currency.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,12 +38,12 @@ export class AppComponent {
     this.eventDisabled = (event);
   }
 
-  public netWorth: number | string = 0;
+  public NetWorth: number | string = 0;
   public HealthInsurance: number = 0;
   public SocialInsurance: number = 0;
   public InjuryInsurance: number = 0;
   public MinimalWage: number = 18900;
-  public ErrorCatchPhrase: string = 'Zadejte mzdu vyšší než je minimální mzda ' + this.MinimalWage + ' Kč!';
+  public ErrorCatchPhrase: string = '';
 
   public TaxDiscount: number = 0;
   public TaxDiscountKids: number = 0;
@@ -59,7 +59,20 @@ export class AppComponent {
   public IncomeTaxRich: number = 131901;
   public IncomeTaxPhrase: string = '(15 %)';
 
+  public HealthInsuranceEmployer: number = 0;
+  public SocialInsuranceEmployer: number = 0;
+  public TotalEmployerExpense: number = 0;
 
+  public NetWorth_F: string = '0';
+  public HealthInsurance_F: string = '0';
+  public SocialInsurance_F: string = '0';
+  public InjuryInsurance_F: string = '0';
+  public IncomeTax_F: string = '0';
+  public TaxDiscount_F: string = '0';
+  public TotalTaxDiscountKids_F: string = '0';
+  public HealthInsuranceEmployer_F: string = '0';
+  public SocialInsuranceEmployer_F: string = '0';
+  public TotalEmployerExpense_F: string = '0';
 
   onClick() {
     // this.calcSelectedDisability();
@@ -72,63 +85,50 @@ export class AppComponent {
     this.countTaxKids();
     this.calcTaxDiscount();
     this.calcNetWorth();
+    this.calcHealthInsuranceEmployer();
+    this.calcSocialInsuranceEmployer();
+    this.calcTotalValueOfPerson();
   }
-
-
-
-  // public calcSelectedDisability() {
-  //   console.log("vybraná možnost: " + this.selectedDisability);
-  //   if (this.selectedDisability === "DO") {
-  //     this.TaxDiscountDisabled = 0;
-  //   } else if(this.selectedDisability ==="D1") {
-  //     this.TaxDiscountDisabled = 210;     
-  //   } else if(this.selectedDisability === "D2") {
-  //     this.TaxDiscountDisabled = 420;     
-  //   } else if(this.selectedDisability === "D3") {
-  //     this.TaxDiscountDisabled = 1345;     
-  //   } else {
-  //     this.TaxDiscountDisabled = 0;
-  //   }
-  // }
-  // public calcSelectedDisability() {
-  //   console.log("vybraná možnost: " + this.selectedDisability);
-  //   this.onSelectDisability(event)
-  // }  
 
   public calcHealthInsurance() {
     if (this.inputWage > this.MinimalWage) {
       this.HealthInsurance = Math.round(this.inputWage * 0.045);
+      this.HealthInsurance_F = this.HealthInsurance.toLocaleString() + ' Kč';
     } else {
-      this.HealthInsurance = 0;
+      this.HealthInsurance_F;
     }
   }
 
   public calcSociaInsurance() {
     if (this.inputWage > this.MinimalWage) {
-    this.SocialInsurance = Math.round(this.inputWage * 0.071);
+    this.SocialInsurance = Math.round(this.inputWage * 0.065);
+    this.SocialInsurance_F = this.SocialInsurance.toLocaleString() + ' Kč';
     } else {
-      this.SocialInsurance = 0;
+      this.SocialInsurance_F;
     }
   }
 
   public calcInjuryInsurance() {
     if (this.inputWage > this.MinimalWage) {
       this.InjuryInsurance = Math.round(this.inputWage * 0.006);
+      this.InjuryInsurance_F = this.InjuryInsurance.toLocaleString() + ' Kč';
       } else {
-        this.SocialInsurance = 0;
+        this.InjuryInsurance_F;
       }
   }
 
   //131901
   public calcIncomeTax() {
-    if (this.inputWage > this.MinimalWage && this.inputWage < this.IncomeTaxRich) {
     this.IncomeTax = Math.round(this.inputWage * 0.15);
+    if (this.inputWage > this.MinimalWage && this.inputWage < this.IncomeTaxRich) {
+    this.IncomeTax;
     } else if(this.inputWage >= this.IncomeTaxRich) {
-    this.IncomeTax = Math.round(this.inputWage * 0.23);
-    this.IncomeTaxPhrase = '(23 %) Solidární daň';
+    this.IncomeTax = Math.round(this.IncomeTax + ((this.inputWage - 131901) * 0.07));
+    this.IncomeTaxPhrase = '(15% + 23 % Solidární daň)';
     } else {
     this.IncomeTax = 0;
     }
+    this.IncomeTax_F = this.IncomeTax.toLocaleString() + ' Kč';
   }
 
   public calcKidsTaxDiscount() {
@@ -157,15 +157,15 @@ export class AppComponent {
 
   public countTaxKids() {
     this.TotalTaxDiscountKids = this.TaxDiscountKids + this.TaxDiscountKidsHandy;
+    this.TotalTaxDiscountKids_F = this.TotalTaxDiscountKids.toLocaleString() + ' Kč';
   }
-
-
 
   public calcTaxDiscount() {
     if(this.inputWage > this.MinimalWage) {
       let kids = this.inputNumKids + this.inputNumKidsHandy;
       this.TaxDiscountDisabled = Number(this.eventDisabled);
       this.TaxDiscount = 2570 + this.TaxDiscountDisabled;
+      this.TaxDiscount_F = this.TaxDiscount.toLocaleString() + ' Kč';
       console.log(this.eventDisabled)
       if (kids < 1) {
         this.TotalTaxDiscount = this.TaxDiscount + this.TaxDiscountDisabled;
@@ -179,20 +179,28 @@ export class AppComponent {
 
   public calcNetWorth() {
     if(this.inputWage > this.MinimalWage) {
-    // this.HealthInsurance = this.inputWage * 0.045;
-    // this.SocialInsurance = this.inputWage * 0.071;
-    // this.IncomeTax = this.inputWage * 0.15;
-    this.netWorth = this.inputWage - this.HealthInsurance - this.SocialInsurance - this.InjuryInsurance - this.IncomeTax + this.TotalTaxDiscount;
-    } else {
-      this.netWorth = this.ErrorCatchPhrase;
-    }
+    this.NetWorth = this.inputWage - this.HealthInsurance - this.SocialInsurance - this.InjuryInsurance - this.IncomeTax + this.TotalTaxDiscount;
+    this.NetWorth_F = this.NetWorth.toLocaleString() + ' Kč';
+    this.ErrorCatchPhrase = '';
+  } else {
+      this.NetWorth_F = '';
+      this.ErrorCatchPhrase = 'Výsledek je nižší než MIN mzda: ' + this.MinimalWage + ' Kč!';
+      }
   }
 
-  // public errorCatchPhase() {
-  //   if (this.inputWage > this.MinimalWage) {
-  //     this.ErrorCatchPhase = '';
-  //   } else {
-  //     this.ErrorCatchPhase;
-  //   }
-  // } 
+  public calcHealthInsuranceEmployer() {
+    this.HealthInsuranceEmployer = Math.round(this.inputWage * 0.09);
+    this.HealthInsuranceEmployer_F = this.HealthInsuranceEmployer.toLocaleString() + ' Kč';
+  }
+
+  public calcSocialInsuranceEmployer() {
+    this.SocialInsuranceEmployer = Math.round(this.inputWage * 0.248);
+    this.SocialInsuranceEmployer_F = this.SocialInsuranceEmployer.toLocaleString() + ' Kč';
+  }
+
+  public calcTotalValueOfPerson() {
+    this.TotalEmployerExpense = this.inputWage + this.HealthInsuranceEmployer + this.SocialInsuranceEmployer;
+    this.TotalEmployerExpense_F = this.TotalEmployerExpense.toLocaleString() + ' Kč';
+  }
 }
+  
