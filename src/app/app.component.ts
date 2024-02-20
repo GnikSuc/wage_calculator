b989d03f201e352ecdd20da108311a9a67d709dd
unknown <suchacek.n@gmail.com>
2024-02-20 19:46:16 +0100

    New branch, I am now focusing on making services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgFor } from '@angular/common';
-import { throwError } from 'rxjs';
 import { __values } from 'tslib';
 
 interface Disability {
